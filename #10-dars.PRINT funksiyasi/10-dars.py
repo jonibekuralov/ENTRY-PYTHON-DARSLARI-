f8baade35f3ba1ac5377b4a5523f530b2f612ead
Jonibek <anonymousbycreated@gmail.com>
2023-03-12 11:47:53 +0500

    Create 10-dars.py

---
+++ #10-dars.PRINT funksiyasi/10-dars.py
@@ -0,0 +1,9 @@
+# Entrybot's Python code
+
+import Entry
+
+def when_start():
+    Entry.print("Hello World")
+    Entry.print("Men EntryBotman")
+    Entry.print("Tanishganmdan xursandman")
+    Entry.clear_print()
